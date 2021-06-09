b17a515b30f1906baff076e21e8d2256f0be8c7b
ChargeAhead <alindomuta@gmail.com>
2021-06-09 21:33:16 +0300

    Push till pom

---
+++ SeleniumCSharpNetCore/pages/HomePage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace SeleniumCSharpNetCore.pages
+{
+	public class HomePage : DriverHelper
+	{
+
+		private IWebElement lnkLogin => Driver.FindElement(By.LinkText("Login"));
+		private IWebElement lnkLogoff => Driver.FindElement(By.LinkText("Log off"));
+
+		public void clickLogin() => lnkLogin.Click();
+
+		public bool isLogoffExist() => lnkLogoff.Displayed;
+
+
+	}
+}
